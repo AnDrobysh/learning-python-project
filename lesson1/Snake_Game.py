852fdedc503516313d845599d0301e4685dff21c
August <ee.480818@gmail.com>
2025-10-15 00:04:56 +0300

    add lesson 2

--- lesson1/Snake_Game.py
+++ lesson1/Snake_Game.py
@@ -1,61 +1,64 @@
 import random
 
-field = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],]
+'''Алгоритм создания поля'''
+field = []
+k = 0
+for k in range(0, 10):
+    field.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 snake = [[0, 0]]
 apple = 0
+
 while True:
+    '''Алгоритм создания яблока если прошлое съедено'''
     if apple == 0:
         x = random.randint(0, 9)
         y = random.randint(0, 9)
         field[x][y] = 2
         apple = 1
-    print(field[0])
-    print(field[1])
-    print(field[2])
-    print(field[3])
-    print(field[4])
-    print(field[5])
-    print(field[6])
-    print(field[7])
-    print(field[8])
-    print(field[9])
 
+    '''Алгоритм вывода поля'''
+    for i in field:
+        print(i)
+
+    '''Алгоритм приема цифры'''
     current_key = 0
     while current_key != '1' and current_key != '2' and current_key != '3' and current_key != '4':
         current_key = input()
     current_key = int(current_key)
+
     if current_key == 1:
         if snake[-1][1] == 9:
-            print('Вы проиграли')
-            print('Score: ', len(snake))
-            break
+            last = snake[0]
+            print(last)
+            snake[-1][1] = 0
+            if field[snake[-1][0]][snake[-1][1] + 1] != 1 and field[snake[-1][0]][snake[-1][1] + 1] != 2:
+                print('popali v tot if')
+                append = [snake[-1][0], snake[-1][1]]
+                snake.append(append)
+                field[last[0]][last[1]] = 0
+                snake.remove(last)
 
-        if field[snake[-1][0]][snake[-1][1] + 1] != 1 and field[snake[-1][0]][snake[-1][1] + 1] != 2:
-            append = [snake[-1][0], snake[-1][1] + 1]
-            snake.append(append)
-            field[snake[0][0]][snake[0][1]] = 0
-            snake.pop(0)
 
-        elif field[snake[-1][0]][snake[-1][1] + 1] == 2:
-            print('попали в elif = 2')
-            append = [snake[-1][0], snake[-1][1] + 1]
-            snake.append(append)
+            elif field[snake[-1][0]][snake[-1][1]] == 2:
+                append = [snake[-1][0], snake[-1][1]]
+                snake.append(append)
 
-        if snake[-1][1] != 9:
-            if field[snake[-1][0]][snake[-1][1] + 1] == 1 or snake[-1][1] < 0:
+            elif field[snake[-1][0]][snake[-1][1]] == 1:
                 print('Вы проиграли')
                 print('Score: ', len(snake))
                 break
+
+        elif snake[-1][1] != 9:
+            if field[snake[-1][0]][snake[-1][1] + 1] != 1 and field[snake[-1][0]][snake[-1][1] + 1] != 2:
+                append = [snake[-1][0], snake[-1][1] + 1]
+                snake.append(append)
+                field[snake[0][0]][snake[0][1]] = 0
+                snake.pop(0)
+
+            elif field[snake[-1][0]][snake[-1][1] + 1] == 2:
+                append = [snake[-1][0], snake[-1][1] + 1]
+                snake.append(append)
 
     if current_key == 2:
         if field[snake[-1][0]][snake[-1][1] - 1] != 1 and field[snake[-1][0]][snake[-1][1] - 1] != 2:
@@ -110,10 +113,12 @@ while True:
             print('Вы проиграли')
             print('Score: ', len(snake))
             break
+
+    '''Алгоритм отображающий змейку на поле'''
     i = 0
-    count = 0
     for i in snake:
         field[i[0]][i[1]] = 1
 
+    '''Алгоритм проверки съедено ли яблоко'''
     if field[x][y] != 2:
         apple = 0
