066837691a8747efd280123449c2ac8e4267b201
August <ee.480818@gmail.com>
2025-10-22 13:00:55 +0300

    wensday

--- lesson1/Snake_game_clean.py
+++ lesson1/Snake_game_clean.py
@@ -1,7 +1,8 @@
 import random
+game_over = False
 field = []
-snake = [[1, 1]]
-is_there_an_apple = False
+snake = [[1, 2], [1, 1]]
+is_there_an_apple = [False]
 
 '''Функция вывода ячейки'''
 def print_cell(cymboll):
@@ -22,14 +23,14 @@ def create_snake(snake, field):
 
 '''Функция создания яблока'''
 def create_apple(is_there_an_apple, field):
-    if not is_there_an_apple:
+    if not is_there_an_apple[0]:
         x = random.randint(1, 10)
         y = random.randint(1, 10)
         cell = get_cell(x, y, field)
         if cell[2]:
             create_apple(is_there_an_apple, field)
         cell[3] = True
-        return field
+        is_there_an_apple[0] = True
 
 '''Функция создания поля'''
 def create_field(x, y):
@@ -49,8 +50,34 @@ def print_field(field):
         if cell[0] % 10 == 0:
             print()
 
+'''Функция передвижения змейки'''
+def snake_move(snake):
+    current_key = input('Введите wasd')
+    if current_key != 's' and current_key != 'w' and current_key != 'a' and current_key != 'd':
+        print('ВВедите корректное значение')
+        snake_move(snake)
+
+    for s in snake:
+        match current_key:
+            case 'w':
+                s[1] += 1
+            case 'a':
+                s[0] -= 1
+            case 'd':
+                s[0] += 1
+            case 's':
+                s[1] -= 1
+
+'''Функция очищения змейки'''
+def clear_snake(field):
+    for cell in field:
+        cell[2] = False
+
 '''Выполнение кода'''
 create_field(10, 10)
-create_snake(snake, field)
-create_apple(is_there_an_apple, field)
-print_field(field)
+while not game_over:
+    create_snake(snake, field)
+    create_apple(is_there_an_apple, field)
+    print_field(field)
+    clear_snake(field)
+    snake_move(snake)
